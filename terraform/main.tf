0c1d9210e867dedc461d697f9b241768d0eb2d6f
getpc2 <getpc2@genians.com>
2022-04-15 12:41:33 +0900

    test1

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0f949cb787308f8a7"
   instance_type = "t2.micro"
 
   tags = {
