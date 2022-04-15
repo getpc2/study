5fbdafdd4b17a6a584b0337c54f71ed69033dd82
getpc2 <getpc2@genians.com>
2022-04-15 12:54:21 +0900

    sg 생성

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region  = "ap-northeast-2"
 }
 
-resource "aws_security_group" "web" {
+resource "aws_security_group" "office ssh" {
   vpc_id = "vpc-0951fb531d20fe80a"
   name = "office ssh"
   description = "Terraform home ssh SG"
@@ -23,11 +23,13 @@ resource "aws_security_group" "web" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["222.121.135.254/32"]
+  }
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0f949cb787308f8a7"
   instance_type = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.basic_security.id]
 
   tags = {
     Name = "hello world 1"
