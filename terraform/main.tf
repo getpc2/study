ffce9cdd677870109b4ac0e8580b10cde46953b3
getpc2 <getpc2@genians.com>
2022-04-15 17:06:24 +0900

    cteate 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "aws_vpc" "jhhtest-vpc" {
   }
 }
 
-resource "aws_subnet" "jhhtest-subnet-pri-2a-1" {
+resource "aws_subnet" "jhhtest-subnet-pri" {
   vpc_id                  = aws_vpc.jhhtest-vpc.id
   cidr_block              = "50.50.1.0/24"
   map_public_ip_on_launch = false
